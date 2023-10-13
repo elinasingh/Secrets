ecaabfa46eca605b8459adddfd8095b641e3b089
Elina <elinasingh1997@gmail.com>
2023-10-13 15:44:20 +0530

    created userDb & saving newUser info after registering

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true, 
+useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -18,6 +29,17 @@ app.get("/login", function(req, res) {
 
 app.get("/register", function(req, res) {
     res.render("register");
+});
+
+app.post("/register", function(req, res) {
+     const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+     });
+
+     newUser.save();
+     console.log(newUser);
+     res.render("secrets");
 });
 
 
